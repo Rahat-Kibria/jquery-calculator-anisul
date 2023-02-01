474e5399361b841fb106723ee8e4589a84eb9ab5
Rahat-Kibria <rahatkibriawahee@gmail.com>
2023-02-01 11:00:59 +0600

    project done

---
+++ index.js
@@ -0,0 +1,22 @@
+function insertNumber(number)
+{
+    var existingNumber=$("#result").val();
+    $("#result").val(existingNumber+number);
+}
+function clearResult()
+{
+    $("#result").val("");
+}
+function calculate()
+{
+    var result=eval($("#result").val());
+    $("#result").val(result);
+}
+function deleteNumber()
+{
+    var presentValue=$("#result").val();
+    if(presentValue!="")
+    {
+        $("#result").val(presentValue.slice(0,-1));
+    }
+}
